70948ad69d3d823a1a5a7bacb48474f04e8d79bd
Loris Lunardi <lunardi.loris@gmail.com>
2021-04-19 12:27:25 +0800

    code challenge init

---
+++ database/migrations/2020_01_28_100000_create_loans_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Loan;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('amount');
+            $table->integer('terms');
+            $table->integer('outstanding_amount')->default(0);
+            $table->string('currency_code');
+            $table->date('processed_at');
+            $table->string('status')->default(Loan::STATUS_DUE);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
